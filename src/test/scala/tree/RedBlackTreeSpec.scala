babb9a564ba32ca3c687ec34a2a7f892ed62568a
Vladislav Pakhomov <d1skortneru@yandex.ru>
2019-07-14 12:09:02 +0300

    add test for second invariant

--- src/test/scala/tree/RedBlackTreeSpec.scala
+++ src/test/scala/tree/RedBlackTreeSpec.scala
@@ -4,7 +4,7 @@ import cats.instances.int._
 import org.scalacheck.Prop
 import org.scalatest.FlatSpec
 import org.scalatestplus.scalacheck.Checkers
-import tree.RedBlackTree.Color.Red
+import tree.RedBlackTree.Color.{ Black, Red }
 import tree.RedBlackTree.{ Branch, Empty }
 
 class RedBlackTreeSpec extends FlatSpec with Checkers {
@@ -17,9 +17,29 @@ class RedBlackTreeSpec extends FlatSpec with Checkers {
       case Empty                                   => true
     }
 
-  it should "not allow for red children be with red parent" in {
+  def checkForPath[A](tree: RedBlackTree[A]): Boolean = {
+    def go(t: RedBlackTree[A]): List[Int] =
+      t match {
+        case Empty                         => List(0)
+        case Branch(Red, left, _, right)   => go(left) ++ go(right)
+        case Branch(Black, left, _, right) => (go(left) ++ go(right)).map(_ + 1)
+      }
+
+    go(tree) match {
+      case Nil     => true
+      case x :: xs => xs.forall(_ === x)
+    }
+  }
+
+  it should "not allow red nodes have red children" in {
     check(Prop.forAll { list: List[Int] =>
       checkForColor(Tree[RedBlackTree].fromList(list))
+    })
+  }
+
+  it should "contains the same number of black nodes for every path from root to empty node" in {
+    check(Prop.forAll { list: List[Int] =>
+      checkForPath(Tree[RedBlackTree].fromList(list))
     })
   }
 }
